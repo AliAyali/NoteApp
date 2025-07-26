b307baae483998200aa7afb51b22436c611687cd
AliAyali <149673166+AliAyali@users.noreply.github.com>
2025-07-26 15:51:40 -0700

    add FloatingActionButton to MainActivity with snackbar action

--- app/src/main/java/com/example/note/MainActivity.kt
+++ app/src/main/java/com/example/note/MainActivity.kt
@@ -3,19 +3,54 @@ package com.example.note
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FabPosition
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.SnackbarDuration
+import androidx.compose.material3.SnackbarHostState
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
+import com.example.note.navigation.NavigationScreen
 import com.example.note.navigation.SetupNavigation
 import com.example.note.ui.theme.NoteTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+
         setContent {
             NoteTheme {
                 val navController = rememberNavController()
                 Scaffold(
+
+                    floatingActionButton = {
+                        FloatingActionButton(
+                            modifier = Modifier.padding(10.dp),
+                            onClick = {
+                                navController.navigate(NavigationScreen.Item.route)
+                            },
+                            containerColor = MaterialTheme.colorScheme.primary,
+                            contentColor = MaterialTheme.colorScheme.onPrimary
+                        ) {
+
+                            Icon(
+                                Icons.Default.Add,
+                                "Add"
+                            )
+
+                        }
+                    },
+                    floatingActionButtonPosition = FabPosition.Start
                 ) { padding ->
                     SetupNavigation(padding, navController)
                 }
