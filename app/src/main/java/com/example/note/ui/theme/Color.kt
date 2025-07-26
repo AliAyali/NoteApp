54dac79cd16d2bc6f6de9868aa739c6b19a4b163
AliAyali <149673166+AliAyali@users.noreply.github.com>
2025-07-26 07:37:56 -0700

    Set up app theme: added colors and Sahel font

--- app/src/main/java/com/example/note/ui/theme/Color.kt
+++ app/src/main/java/com/example/note/ui/theme/Color.kt
@@ -2,10 +2,16 @@ package com.example.note.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Light Theme Colors
+val LightPrimaryYellow = Color(0xFFFFC107)
+val LightSecondaryGray = Color(0xFF757575)
+val LightTertiaryYellow = Color(0xFFFFE082)
+val LightBackground = Color(0xFFFAFAFA)
+val LightSurface = Color(0xFFFFFFFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+// Dark Theme Colors
+val DarkPrimaryYellow = Color(0xFFFFC107)
+val DarkSecondaryGray = Color(0xFFBDBDBD)
+val DarkTertiaryYellow = Color(0xFFFFE082)
+val DarkBackground = Color(0xFF121212)
+val DarkSurface = Color(0xFF1E1E1E)
