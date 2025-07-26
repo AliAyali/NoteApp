87b0b3bf2665490390392152c74b4bc19b02769b
AliAyali <149673166+AliAyali@users.noreply.github.com>
2025-07-26 15:06:59 -0700

    Add search box with filtering and clear icon functionality

--- app/src/main/java/com/example/note/presentation/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/note/presentation/screen/home/HomeScreen.kt
@@ -2,29 +2,46 @@ package com.example.note.presentation.screen.home
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Icon
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Tab
 import androidx.compose.material3.TabRow
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.note.R
 
 @Preview
@@ -86,6 +103,52 @@ fun HomeScreen() {
 
         }
 
+        val allItems = listOf(listOf("test1", "text", "1401"), listOf("test2", "text", "1402"))
+
+        var query by remember { mutableStateOf("") }
+        val filtersItems = remember(query) {
+            if (query.isBlank()) allItems
+            else allItems.filter { itemList ->
+                itemList.any { str -> str.contains(query, ignoreCase = true) }
+            }
+        }
+
+        TextField(
+            value = query,
+            onValueChange = { query = it },
+            placeholder = {
+                Text(
+                    text = if (selectedTabIndex == 1) "جست و جوی یادداشت ها"
+                    else "جست و جوی کارها",
+                    modifier = Modifier.fillMaxWidth(),
+                    textAlign = TextAlign.End
+                )
+            },
+            trailingIcon = {
+                Icon(
+                    imageVector = Icons.Default.Search,
+                    contentDescription = "Clear",
+                    modifier = Modifier.clickable {
+                        query = ""
+                    }
+                )
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(75.dp)
+                .padding(10.dp)
+                .clip(CircleShape),
+            colors = TextFieldDefaults.colors(
+                focusedIndicatorColor = Color.Transparent,
+                unfocusedIndicatorColor = Color.Transparent,
+                disabledIndicatorColor = Color.Transparent,
+                focusedContainerColor = MaterialTheme.colorScheme.surface,
+                unfocusedContainerColor = MaterialTheme.colorScheme.surface,
+                unfocusedTextColor = MaterialTheme.colorScheme.surface,
+            ),
+            textStyle = LocalTextStyle.current.copy(textAlign = TextAlign.End)
+        )
+
 
         when (selectedTabIndex) {
             0 -> {
@@ -93,7 +156,34 @@ fun HomeScreen() {
             }
 
             1 -> {
-                Text("Tab 2")
+                if (filtersItems.isNotEmpty())
+                    LazyColumn {
+                        items(filtersItems) { it ->
+                            NoteItem(title = it[0], detail = it[1], date = it[2])
+                        }
+                    }
+                else
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Icon(
+                            painter = painterResource(R.drawable.ic_add_notes),
+                            null,
+                            modifier = Modifier.size(80.dp),
+                            tint = MaterialTheme.colorScheme.secondary
+                        )
+
+                        Spacer(Modifier.height(20.dp))
+
+                        Text(
+                            text = "هیچ یادداشتی موجود نیست",
+                            fontWeight = FontWeight.Bold,
+                            fontSize = 25.sp,
+                            color = MaterialTheme.colorScheme.secondary
+                        )
+                    }
             }
         }
     }
